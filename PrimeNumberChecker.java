7b602012aa4d7f55eac47bf6073fa357476028cc
lewisJam3s <157049784+lewisJam3s@users.noreply.github.com>
2025-07-02 19:00:02 -0400

    changed hardcoded number

--- PrimeNumberChecker.java
+++ PrimeNumberChecker.java
@@ -1,6 +1,6 @@
 public class PrimeNumberChecker {
     public static void main(String[] args) {
-        int number = 41; // Hardcoded input number
+        int number = 98; // Hardcoded input number
 
         boolean isPrime = true;
 
